61443339089fef28cdab90af40429d905ef1fb49
vassmate <vassmate08@gmail.com>
2016-04-19 12:30:01 +0200

    Adding some task files

---
+++ OrganizeStrings/RunOrganizeStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Workshop_Week14b
+{
+    class RunOrganizeStrings
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>();
+            
+            Console.WriteLine(">Press ESC to stop adding strings!");
+            Console.WriteLine(">Write something below.");
+            do
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                stringList.Add(input);
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            stringList.Sort();
+
+            Console.Write(">Sorted strings: ");
+            foreach (string stuff in stringList)
+            {
+                Console.Write(stuff + ",");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(">Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReverseIntegers/ReverseIntegers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseIntegers
+{
+    class ReverseIntegers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int>();
+
+            Console.WriteLine(">Press (ESC) to stop adding strings!");
+            Console.WriteLine(">Write something below.");
+
+            do
+            {
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                numberList.Add(int.Parse(input));
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            numberList.Sort();
+            
+            Stack<int> reversedNumberStack = new Stack<int>();
+            foreach (int i in numberList)
+            {
+                reversedNumberStack.Push(i);
+            }
+            
+            while(reversedNumberStack.Count > 0)
+            {
+                object obj = reversedNumberStack.Pop();
+                Console.WriteLine(" >From stack: {0}", obj);
+            }
+            
+            Console.WriteLine(">Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
